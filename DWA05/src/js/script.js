7286a0a47c11e0b56ca2d0168c0e9d2dc7ee8945
Kealeboga Letlhake <letlhake.kv@gmail.com>
2023-06-05 15:22:51 +0200

    Update script.js

--- DWA05/src/js/script.js
+++ DWA05/src/js/script.js
@@ -1,5 +1,3 @@
-
-
 const form = document.querySelector("[data-form]");
 const result = document.querySelector("[data-result]");
 
@@ -7,69 +5,28 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
-});
 
-
-
-//Scenario: Validation when values are missing
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const entries = new FormData(event.target);
-  const { dividend, divider } = Object.fromEntries(entries);
-
+  //Scenario: Validation when values are missing
   if (!dividend || !divider) {
     result.innerText = "Division not performed. Both values are required in inputs. Try again";
     return;
   }
-
-  const divisionResult = dividend / divider;
-  const wholeNumber = Math.floor(divisionResult);
-  result.innerText = wholeNumber;
-});
-
+//Scenario: Providing anything that is not a number should crash the program
+  if (isNaN(dividend) || isNaN(divider)) {
+    console.error("Critical error: Non-numeric input provided");
+    document.body.innerHTML = "<h1>Something critical went wrong. Please reload the page</h1>";
+    return;
+  }
 //Scenario: An invalid division should log an error in the console
-
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const entries = new FormData(event.target);
-  const { dividend, divider } = Object.fromEntries(entries);
-
-  if (!dividend || !divider) {
-    result.innerText = "Division not performed. Both values are required in inputs. Try again";
-    return;
-  }
-
-  if ( divider < 0) {
-    result.innerText = "Something critical went wrong. Please reload the page";
+  if (divider < 0) {
     console.error("Invalid input provided");
     console.trace();
     document.body.innerHTML = "<h1>Division not performed. Invalid number provided. Try again</h1>";
     return;
   }
 
+  //Scenario: Dividing numbers result in a decimal number
   const divisionResult = dividend / divider;
   const wholeNumber = Math.floor(divisionResult);
-  result.innerText = wholeNumber;
-});
-
-//Scenario: Providing anything that is not a number should crash the program
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const entries = new FormData(event.target);
-  const { dividend, divider } = Object.fromEntries(entries);
-
-  if (isNaN(dividend) || isNaN(divider)) {
-    console.error("Critical error: Non-numeric input provided");
-    document.body.innerHTML = "<h1>Something critical went wrong. Please reload the page</h1>";
-    return;
-  }
-
-  const divisionResult = dividend / divider;
-  const wholeNumber = Math.floor(divisionResult);
-  const result = document.querySelector("[data-result]");
   result.innerText = wholeNumber;
 });
