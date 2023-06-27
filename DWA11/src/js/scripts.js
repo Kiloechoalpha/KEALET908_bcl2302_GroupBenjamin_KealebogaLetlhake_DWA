258476929ed00c0577b104c1b7c1bb47b5b7fc59
Kealeboga Letlhake <letlhake.kv@gmail.com>
2023-06-27 09:56:47 +0200

    Update File

--- DWA11/src/js/scripts.js
+++ DWA11/src/js/scripts.js
@@ -2,6 +2,7 @@ const MAX_NUMBER = 15;
 const MIN_NUMBER = -5;
 const STEP_AMOUNT = 1;
 
+
 const number = document.querySelector('[data-key="number"]')
 const subtract = document.querySelector('[data-key="subtract"]')
 const add = document.querySelector('[data-key="add"]')
@@ -36,11 +37,12 @@ const addHandler = () => {
 //added a handleReset button to reset tally count to 0
 const handleReset = () => {
     number.value = 0;
-    subtract.disabled = false;
+    subtract.disabled = true;
     add.disabled = false;
     //added alert message for when the reset button has been clicked
     window.alert("Counter is resetting back to 0")
 };
+
 
 subtract.addEventListener('click', subtractHandler);
 add.addEventListener('click',  addHandler);
